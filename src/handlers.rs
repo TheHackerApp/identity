096ccf367c64e844a6d1b2ee58da879e2ee8db76
Alex Krantz <alex@krantz.dev>
2023-09-08 14:50:09 -0700

    Add route for fetching user context

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,9 +1,20 @@
-use crate::{oauth, AppState};
+use crate::{oauth, session, AppState};
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::routing::get;
-use axum::{extract::State, response::Html, Router};
+use axum::{
+    extract::{Query, State},
+    response::{Html, Json},
+    routing::get,
+    Router,
+};
+use database::{PgPool, User};
+use serde::{Deserialize, Serialize};
 use tracing::instrument;
+
+mod error;
+
+use crate::session::SessionState;
+use error::Result;
 
 /// Create router for handling OAuth
 pub(crate) fn oauth() -> Router<AppState> {
@@ -24,4 +35,78 @@ pub(crate) async fn graphql(State(state): State<AppState>, req: GraphQLRequest) 
 pub(crate) async fn playground() -> Html<String> {
     let config = GraphQLPlaygroundConfig::new("/graphql").title("Identity Playground");
     Html(playground_source(config))
+}
+
+/// Get the user context for the request
+#[instrument(name = "context", skip_all)]
+pub(crate) async fn context(
+    Query(params): Query<ContextParams>,
+    State(db): State<PgPool>,
+    State(sessions): State<session::Manager>,
+) -> Result<Json<Option<UserInfo>>> {
+    let Some(session) = sessions.load_from_token(&params.token).await? else {
+        return Ok(Json(None));
+    };
+
+    let user_info = match session.state {
+        SessionState::Unauthenticated => UserInfo::Unauthenticated,
+        SessionState::OAuth(_) => UserInfo::OAuth,
+        SessionState::RegistrationNeeded(state) => UserInfo::RegistrationNeeded {
+            id: state.id,
+            email: state.email,
+        },
+        SessionState::Authenticated(state) => {
+            let user = User::find(state.id, &db).await?.expect("user must exist");
+
+            // TODO: determine permissions
+
+            UserInfo::Authenticated {
+                id: user.id,
+                given_name: user.given_name,
+                family_name: user.family_name,
+                email: user.primary_email,
+                is_admin: user.is_admin,
+            }
+        }
+    };
+
+    Ok(Json(Some(user_info)))
+}
+
+/// The parameters for fetching the user context
+#[derive(Deserialize)]
+pub(crate) struct ContextParams {
+    /// The session token
+    token: String,
+}
+
+/// The generate user context
+#[derive(Serialize)]
+#[serde(tag = "type", rename_all = "kebab-case")]
+pub(crate) enum UserInfo {
+    /// The user is currently unauthenticated
+    Unauthenticated,
+    /// The user is in the middle of logging in via OAuth
+    #[serde(rename = "oauth")]
+    OAuth,
+    /// The user needs to complete their registration
+    RegistrationNeeded {
+        /// The user's ID according to the provider
+        id: String,
+        /// The user's primary email
+        email: String,
+    },
+    /// The user is fully authenticated
+    Authenticated {
+        /// The user's ID
+        id: i32,
+        /// The user's given/first name
+        given_name: String,
+        /// The user's family/last name
+        family_name: String,
+        /// The user's primary email
+        email: String,
+        /// Whether the user is an admin
+        is_admin: bool,
+    },
 }
---
+++ src/handlers/error.rs
@@ -0,0 +1,83 @@
+use crate::session;
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Json, Response},
+};
+use serde::Serialize;
+use std::fmt::{Display, Formatter};
+use tracing::error;
+
+pub(crate) type Result<T, E = Error> = std::result::Result<T, E>;
+
+/// Errors that can occur in request handlers
+#[derive(Debug)]
+pub(crate) enum Error {
+    Database(database::Error),
+    Session(session::Error),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Database(_) => write!(f, "unexpected database error"),
+            Self::Session(_) => write!(f, "unexpected session error"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Database(e) => Some(e),
+            Self::Session(e) => Some(e),
+        }
+    }
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        use std::error::Error as _;
+
+        match self {
+            Self::Database(error) => match error.source() {
+                Some(source) => error!(%error, %source, "unexpected database error"),
+                None => error!(%error, "unexpected database error"),
+            },
+            Self::Session(error) => match error.source() {
+                Some(source) => error!(%error, %source, "unexpected session error"),
+                None => error!(%error, "unexpected session error"),
+            },
+        };
+
+        ApiError::internal_server_error()
+    }
+}
+
+impl From<database::Error> for Error {
+    fn from(error: database::Error) -> Self {
+        Self::Database(error)
+    }
+}
+
+impl From<session::Error> for Error {
+    fn from(error: session::Error) -> Self {
+        Self::Session(error)
+    }
+}
+
+#[derive(Serialize)]
+struct ApiError {
+    message: &'static str,
+}
+
+impl ApiError {
+    fn internal_server_error() -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(ApiError {
+                message: "internal server error",
+            }),
+        )
+            .into_response()
+    }
+}
