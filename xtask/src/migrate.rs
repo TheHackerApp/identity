958b27f7be1dc530570315851b26997c9164623b
Alex Krantz <alex@krantz.dev>
2023-09-08 00:18:35 -0700

    Use migrator crate from private registry

--- xtask/src/migrate.rs
+++ xtask/src/migrate.rs
@@ -1,11 +1,29 @@
+use eyre::WrapErr;
+use sqlx::{
+    migrate::Migrator,
+    postgres::{PgConnectOptions, PgPool},
+    ConnectOptions,
+};
+use std::{path::PathBuf, str::FromStr};
+use tracing::log::LevelFilter;
+
 pub async fn run(args: Args) -> eyre::Result<()> {
-    let db = database::connect(&args.database_url).await?;
+    let migrator = Migrator::new(&*args.source)
+        .await
+        .wrap_err("failed to load migrations")?;
+
+    let options = PgConnectOptions::from_str(&args.database_url)
+        .wrap_err("invalid database URL format")?
+        .log_statements(LevelFilter::Debug);
+    let db = PgPool::connect_with(options)
+        .await
+        .wrap_err("failed to connect to the database")?;
 
     match args.command {
-        Command::Add { name } => migrator::add(name.join("_"))?,
-        Command::Info => migrator::info(&db).await?,
-        Command::Apply => migrator::apply(&db).await?,
-        Command::Revert { target } => migrator::undo(&db, target).await?,
+        Command::Add { name } => migrator::add(&args.source, &name.join("_"))?,
+        Command::Info => migrator::info(&migrator, &db).await?,
+        Command::Apply => migrator::apply(&migrator, &db).await?,
+        Command::Revert { target } => migrator::undo(&migrator, &db, target).await?,
     }
 
     Ok(())
@@ -16,6 +34,10 @@ pub struct Args {
     /// The database to run migrations on
     #[arg(short, long, env = "DATABASE_URL")]
     database_url: String,
+
+    /// The migrations source
+    #[arg(short, long, default_value = "./migrations")]
+    source: PathBuf,
 
     #[command(subcommand)]
     command: Command,
